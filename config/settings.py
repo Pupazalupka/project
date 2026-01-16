94676cc5dcbb053dd8136f3934a397d2d48157f8
Arina <kostovar1601@gmail.com>
2026-01-16 17:14:32 +0500

    исправления деплоя

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-for-dev')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'hikeweather-advisor.onrender.com',
+    'project-zzo7.onrender.com',
     'localhost',
     '127.0.0.1',
     '0.0.0.0',
